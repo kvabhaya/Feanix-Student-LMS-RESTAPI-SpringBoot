13209e346a021e81e741f209751fcda860f74605
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-07-19 15:50:42 +0530

    created files

---
+++ src/test/java/com/devstack/lms/feanix/FeanixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstack.lms.feanix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeanixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
