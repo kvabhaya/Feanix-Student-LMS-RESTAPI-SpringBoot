eab2fa1d1a3a452a803a1e27e731efa2f36504d9
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-07-22 12:27:36 +0530

    Updated ProductServiceImpl.java

--- src/main/java/com/devstack/lms/feanix/service/impl/ProductServiceImpl.java
+++ src/main/java/com/devstack/lms/feanix/service/impl/ProductServiceImpl.java
@@ -1,15 +1,33 @@
 package com.devstack.lms.feanix.service.impl;
 
 import com.devstack.lms.feanix.dto.request.RequestProductDto;
+import com.devstack.lms.feanix.entity.Product;
+import com.devstack.lms.feanix.repository.ProductRepository;
 import com.devstack.lms.feanix.service.ProductService;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @Transactional //table dekakata ekapara value dagann
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
+    private final ProductRepository repository;
     @Override
     public void create(RequestProductDto requestProductDto) {
-
+        repository.save(toProduct(requestProductDto));
+    }
+    private Product toProduct(RequestProductDto dto){
+        if(dto==null) return null;
+        return Product.builder()
+                .productId(UUID.randomUUID().toString())
+                .description(dto.getDescription())
+                .fileResource(null)
+                .orderDetails(null)
+                .qtyOnHand(dto.getQtyOnHand())
+                .unitPrice(dto.getUnitPrice())
+                .build();
     }
 }
