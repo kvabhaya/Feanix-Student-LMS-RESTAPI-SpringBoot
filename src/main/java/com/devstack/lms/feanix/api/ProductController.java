58dfe0b4cb824f9d015733862bd076fba9bba50d
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-07-23 15:47:16 +0530

    Implemented to get StandardResponses

--- src/main/java/com/devstack/lms/feanix/api/ProductController.java
+++ src/main/java/com/devstack/lms/feanix/api/ProductController.java
@@ -4,7 +4,10 @@ import com.devstack.lms.feanix.dto.paginate.ResponseProductPaginateDto;
 import com.devstack.lms.feanix.dto.request.RequestProductDto;
 import com.devstack.lms.feanix.dto.response.ResponseProductDto;
 import com.devstack.lms.feanix.service.ProductService;
+import com.devstack.lms.feanix.util.StandardResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,32 +16,47 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
     private final ProductService productService;
     @PostMapping
-    public String save(@RequestBody RequestProductDto dto){
+    public ResponseEntity<StandardResponseDto> save(@RequestBody RequestProductDto dto){
         productService.create(dto);
-        return "Saved";
+        return new ResponseEntity<>(
+                new StandardResponseDto("product saved", 201, null),
+                HttpStatus.CREATED
+        );
     }
 
     @GetMapping("/{productId}")
-    public ResponseProductDto findById(@PathVariable String productId){
-        return productService.findById(productId);
+    public ResponseEntity<StandardResponseDto> findById(@PathVariable String productId) {
+        return new ResponseEntity<>(
+                new StandardResponseDto("product selected", 200, productService.findById(productId)),
+                HttpStatus.OK
+        );
     }
 
     @PutMapping("/{productId}")
-    public String update(@RequestBody RequestProductDto dto, @PathVariable String productId){
+    public ResponseEntity<StandardResponseDto> update(@RequestBody RequestProductDto dto, @PathVariable String productId){
         productService.update(dto,productId);
-        return "Updated";
+        return new ResponseEntity<>(
+                new StandardResponseDto("product modified", 200, null),
+                HttpStatus.OK
+        );
     }
     @DeleteMapping("/{productId}")
-    public String delete(@PathVariable String productId){
+    public ResponseEntity<StandardResponseDto> delete(@PathVariable String productId){
         productService.delete(productId);
-        return "Deleted";
+        return new ResponseEntity<>(
+                new StandardResponseDto("product deleted", 204, null),
+                HttpStatus.NOT_FOUND
+        );
     }
     @GetMapping("/search")
-    public ResponseProductPaginateDto search(
+    public ResponseEntity<StandardResponseDto> search(
             @RequestParam String searchText,
             @RequestParam int page,
             @RequestParam int size
     ){
-        return productService.search(searchText,page,size);
+        return new ResponseEntity<>(
+                new StandardResponseDto("product list", 200, productService.search(searchText,page,size)),
+                HttpStatus.OK
+        );
     }
 }
