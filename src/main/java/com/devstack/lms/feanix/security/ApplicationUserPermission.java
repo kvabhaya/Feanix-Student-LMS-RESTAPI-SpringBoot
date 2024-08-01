b7299e9e7292aa53f0b0cdd5f6c73705a89d26be
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-08-01 19:15:10 +0530

    Updated ApplicationUserPermission

--- src/main/java/com/devstack/lms/feanix/security/ApplicationUserPermission.java
+++ src/main/java/com/devstack/lms/feanix/security/ApplicationUserPermission.java
@@ -1,4 +1,18 @@
 package com.devstack.lms.feanix.security;
 
 public enum ApplicationUserPermission {
+    PRODUCT_READ ("product : read"),
+    PRODUCT_WRITE ("product : write"),
+    ORDER_WRITE ("order : write"),
+    ORDER_READ ("order : read"),
+    USER_ROLE_WRITE("userRole : write");
+
+    private final String permission;
+
+    ApplicationUserPermission (String permission){
+        this.permission = permission;
+    }
+    public String getPermission(){
+        return permission;
+    }
 }
