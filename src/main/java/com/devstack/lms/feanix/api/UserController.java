3c5bcc01ff8116459d2f5fb4b705daadfbb59139
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-08-02 23:01:10 +0530

    signUp method

--- src/main/java/com/devstack/lms/feanix/api/UserController.java
+++ src/main/java/com/devstack/lms/feanix/api/UserController.java
@@ -1,16 +1,22 @@
 package com.devstack.lms.feanix.api;
 
+import com.devstack.lms.feanix.dto.request.RequestUserDto;
 import com.devstack.lms.feanix.service.ApplicationUserService;
 import com.devstack.lms.feanix.util.StandardResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/users")
 public class UserController {
     private final ApplicationUserService applicationUserService;
-    public ResponseEntity<StandardResponseDto> signup(){}
+    @PostMapping("/signup")
+    public ResponseEntity<StandardResponseDto> signup(@RequestBody RequestUserDto dto){
+        applicationUserService.create(dto);
+        return new ResponseEntity<>(new StandardResponseDto("user created..",201,dto.getUsername()),
+                HttpStatus.CREATED);
+    }
 }
