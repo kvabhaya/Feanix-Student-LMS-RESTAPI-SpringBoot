55dea3b8bc7a5ad118bd3b2423b3b44f3ca14a7a
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-08-02 22:39:57 +0530

    updated FeanixApplication

--- src/main/java/com/devstack/lms/feanix/FeanixApplication.java
+++ src/main/java/com/devstack/lms/feanix/FeanixApplication.java
@@ -1,13 +1,18 @@
 package com.devstack.lms.feanix;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FeanixApplication {
+public class FeanixApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeanixApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
